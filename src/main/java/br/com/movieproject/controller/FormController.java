434945f0fee4fc8edbf26e92bd90909547edb280
Jose Robson <joserobsonsiqueira23@hotmail.com>
2023-04-15 21:47:29 -0300

    feat: get all data form

--- src/main/java/br/com/movieproject/controller/FormController.java
+++ src/main/java/br/com/movieproject/controller/FormController.java
@@ -2,12 +2,15 @@ package br.com.movieproject.controller;
 
 import br.com.movieproject.dto.FormDTO;
 import br.com.movieproject.service.FormService;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "api/v2/form")
@@ -17,9 +20,19 @@ public class FormController {
     @Autowired
     private FormService service;
 
+    @Autowired
+    private ModelMapper mapper;
+
     @PostMapping
     public ResponseEntity<FormDTO> create(@RequestBody FormDTO obj) {
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(service.create(obj).getId()).toUri();
         return ResponseEntity.created(uri).build();
+    }
+
+
+    @GetMapping
+    public ResponseEntity<List<FormDTO>> findAll() {
+        List<FormDTO> listDTO = service.findAll().stream().map(x -> mapper.map(x, FormDTO.class)).collect(Collectors.toList());
+        return ResponseEntity.ok().body(listDTO);
     }
 }
--- src/main/java/br/com/movieproject/service/FormService.java
+++ src/main/java/br/com/movieproject/service/FormService.java
@@ -7,6 +7,8 @@ import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class FormService {
 
@@ -19,5 +21,10 @@ public class FormService {
     public Form create(FormDTO obj) {
 
         return repository.save(mapper.map(obj, Form.class));
+    }
+
+    public List<Form> findAll() {
+
+        return repository.findAll();
     }
 }
